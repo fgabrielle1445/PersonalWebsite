505570ebad590d5736965c8960cfeed5f78c8f05
Gabby <fgabrielle1445@yahoo.com>
2021-12-11 22:26:46 -0600

    Changed some header things

--- personal-website/src/components/Header.js
+++ personal-website/src/components/Header.js
@@ -1,27 +1,29 @@
-import {Link} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { Tabs } from 'antd';
+import "antd/dist/antd.css";
 import './header.css';
 
-function Header() {
+ function Header() {
+  const { TabPane } = Tabs;
+  const navigate = useNavigate();
+  
+  function handleClick(key) {
+    navigate(key);
+  }
+
     return(
         <nav style={{ margin: 10 }}>
         <div className='div-header'>
-        <Link to="/" style={{ padding: 5 }}>
-          Home
-        </Link>
-        <Link to="/about" style={{ padding: 5 }}>
-          About
-        </Link>
-        <Link to="/contact" style={{ padding: 5 }}>
-          Contact
-        </Link>
-        <Link to="/resume" style={{ padding: 5 }}>
-          Resume
-        </Link>
-        <Link to="/projects" style={{ padding: 5 }}>
-          Projects
-        </Link>
+
+        <Tabs defaultActiveKey="/" onChange={handleClick}>
+    <TabPane tab="Home" key="/" />
+    <TabPane tab="About" key="/about" />
+    <TabPane tab="Contact" key="/contact" />
+    <TabPane tab="Resume" key="/resume" />
+    <TabPane tab="Projects" key="/projects" />
+  </Tabs>
         </div>
-      </nav>
+      </nav>      
     );
 }
 
